0c433b65c6a1c9e0abe94b75bfdb4de8c77ba599
Fede Herrera <herrera3299@gmail.com>
2024-10-31 23:52:52 -0300

    scrollreveal is added and update readme

---
+++ src/assets/scrollanimation.js
@@ -0,0 +1,33 @@
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('.t-left', {
+	distance: '20px',
+	duration: 2000,
+	easing: 'ease-in-out',
+	origin: 'left',
+	delay:300
+});
+
+ScrollReveal().reveal('.hero', {
+	distance: '20px',
+	duration: 2000,
+	easing: 'ease-in-out',
+	origin: 'right',
+	delay:300
+});
+
+ScrollReveal().reveal('.navbar', {
+    distance: '20px',
+    duration: 1000,
+    easing: 'ease-out',
+    origin: 'top',
+    delay:100
+  });
+
+  ScrollReveal().reveal('.animation-top', {
+    distance: '30px',
+    duration: 2000,
+    easing: 'ease-out',
+    origin: 'top',
+    delay:300
+  });
